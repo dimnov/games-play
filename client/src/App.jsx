3f7705a4377c5c242a743e2533a943877acff742
dimnov <dimnov1512@gmail.com>
2023-11-17 16:58:11 +0200

    Add create game component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
 import Catalog from "./components/catalog/Catalog.jsx";
+import CreateGame from "./components/create-game/CreateGame.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/games" element={<Catalog />} />
+        <Route path="/games/create" element={<CreateGame />} />
       </Routes>
     </div>
   );
