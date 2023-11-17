4c8ff80399cd95033fcc7ab4c901c3d8f2558e4c
dimnov <dimnov1512@gmail.com>
2023-11-17 16:46:26 +0200

    Add router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import Header from "./components/header/Header.jsx";
+import Home from "./components/home/Home.jsx";
+import Catalog from "./components/catalog/Catalog.jsx";
 
 function App() {
   return (
     <div id="box">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Catalog />} />
+      </Routes>
     </div>
   );
 }
