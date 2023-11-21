480a8d7ff7d5225019584d27816d635960b52aef
dimnov <dimnov1512@gmail.com>
2023-11-21 23:05:13 +0200

    Implement useForm in Login

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm.js";
 
 export default function Login() {
+  const { values, onChange, onSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+
   return (
     <section id="login-page" className="auth">
-      <form id="login">
+      <form id="login" onSubmit={onSubmit}>
         <div className="container">
           <div className="brand-logo"></div>
           <h1>Login</h1>
@@ -13,11 +19,24 @@ export default function Login() {
             id="email"
             name="email"
             placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values.email}
           />
 
           <label htmlFor="login-pass">Password:</label>
-          <input type="password" id="login-password" name="password" />
-          <input type="submit" className="btn submit" value="Login" />
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            onChange={onChange}
+            value={values.password}
+          />
+          <input
+            type="submit"
+            className="btn submit"
+            value="Login"
+            onChange={onChange}
+          />
           <p className="field">
             <span>
               If you don't have profile click <Link to="register">here</Link>
