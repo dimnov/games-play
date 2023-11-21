c8fb6d552d57b33a4a761fcdc33c75912fba7a4b
dimnov <dimnov1512@gmail.com>
2023-11-22 00:04:19 +0200

    Implement register page with working inputs

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm.js";
 
 const RegisterFormKeys = {
   Email: "email",
   Password: "password",
+  ConfirmPassword: "confirm-password",
 };
 
 export default function Register({ registerSubmitHandler }) {
@@ -13,7 +15,7 @@ export default function Register({ registerSubmitHandler }) {
 
   return (
     <section id="register-page" className="content auth">
-      <form id="register">
+      <form id="register" onSubmit={onSubmit}>
         <div className="container">
           <div className="brand-logo"></div>
           <h1>Register</h1>
@@ -22,18 +24,28 @@ export default function Register({ registerSubmitHandler }) {
           <input
             type="email"
             id="email"
-            name="email"
+            name={RegisterFormKeys.Email}
             placeholder="maria@email.com"
+            onChange={onChange}
+            value={values[RegisterFormKeys.value]}
           />
 
           <label htmlFor="pass">Password:</label>
-          <input type="password" name="password" id="register-password" />
+          <input
+            type="password"
+            name={RegisterFormKeys.Password}
+            id="register-password"
+            onChange={onChange}
+            value={values[RegisterFormKeys.value]}
+          />
 
           <label htmlFor="con-pass">Confirm Password:</label>
           <input
             type="password"
-            name="confirm-password"
+            name={RegisterFormKeys.ConfirmPassword}
             id="confirm-password"
+            onChange={onChange}
+            value={values[RegisterFormKeys.value]}
           />
 
           <input className="btn submit" type="submit" value="Register" />
